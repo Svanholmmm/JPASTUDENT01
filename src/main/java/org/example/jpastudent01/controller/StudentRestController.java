f6b09a96d8cff90b2bec033140d64772c3a3dc5c
svanholm200-debug <svanholm200@gmail.com>
2026-08-31 09:40:39 +0200

    More restApi with postman and restcontroller methods

--- src/main/java/org/example/jpastudent01/controller/StudentRestController.java
+++ src/main/java/org/example/jpastudent01/controller/StudentRestController.java
@@ -2,12 +2,13 @@ package org.example.jpastudent01.controller;
 
 import org.example.jpastudent01.model.Student;
 import org.example.jpastudent01.repositories.StudentRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentRestController {
@@ -18,20 +19,52 @@ public class StudentRestController {
     }
 
     @GetMapping("/students")
-    public List<Student> students(){
+    public List<Student> students() {
         var obj = studentRepository.findAll();
         return obj;
     }
+
     @GetMapping("/addStudent")
-    public List<Student> addStudent(){
+    public List<Student> addStudent() {
         Student std = new Student();
         std.setBornDate(LocalDate.now());
-      var obj = studentRepository.findAll();
-      return obj;
+        var obj = studentRepository.findAll();
+        return obj;
     }
+
     @GetMapping("students/{name}")
-public List<Student> getAllStudentsByName(@PathVariable String name){
+    public List<Student> getAllStudentsByName(@PathVariable String name) {
         return studentRepository.findAllByName(name);
     }
 
+    @PostMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student postStudent(@RequestBody Student student) {
+        System.out.println(student);
+        return studentRepository.save(student);
+    }
+
+    @PutMapping("/student")
+    public ResponseEntity<Student> putStudent(@RequestBody Student student) {
+        Optional<Student> orgStudent = studentRepository.findById(student.getId());
+        if (orgStudent.isPresent()) {
+            studentRepository.save(student);
+            return new ResponseEntity<>(student, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/students/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable int id) {
+        Optional<Student> orgStudent = studentRepository.findById(id);
+        if (orgStudent.isPresent()) {
+            studentRepository.deleteById(id);
+            return ResponseEntity.ok("Student deleted");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found");
+        }
+    }
 }
+
+
